e07bfc7c4275d4de42c1f666c43ceac23882de9e
JaneKhris <efkhrisanfova@gmail.com>
2025-06-11 17:32:29 +0300

    correct base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/clovery_jobense",
+  base: "/clovery_jobsense",
   plugins: [react()],
 })
